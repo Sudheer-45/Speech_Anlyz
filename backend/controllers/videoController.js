927f8825c32e0e4530fdc06f0782f88a80f7952e
Sai Sudheer <saisudheermanukonda119@gmail.com>
2025-06-28 02:27:01 +0530

    Update videoController.js

--- backend/controllers/videoController.js
+++ backend/controllers/videoController.js
@@ -5,9 +5,6 @@ const asyncHandler = require('express-async-handler');
 const cloudinary = require('cloudinary').v2;
 const crypto = require('crypto');
 
-
-const { runAnalysisPipeline } = require('../utils/analysisPipeline');
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
